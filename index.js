4b1df1005b63b1562d3b0aa907b7730455b9dcb0
Mark Fridman <marky.fridman@gmail.com>
2017-03-15 16:09:27 +0200

    scroll fix

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 'use strict'
 
 console.log('Protfolio');
+function myFunc(event) {
+	if(event > 400) {
+		jQuery(document).ready(function(){
+			jQuery('.skillbar').each(function(){
+				jQuery(this).find('.skillbar-bar').animate({
+					width:jQuery(this).attr('data-percent')
+				},1500);
+			});
+		});
+	}
+	
 
-jQuery(document).ready(function(){
-	jQuery('.skillbar').each(function(){
-		jQuery(this).find('.skillbar-bar').animate({
-			width:jQuery(this).attr('data-percent')
-		},1500);
-	});
-});
+}
+
